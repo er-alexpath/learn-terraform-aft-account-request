48bbb704be951d5400c94bab89f16eb5d0814d03
Alex <er.alexpath@gmail.com>
2022-08-27 18:31:50 +0530

    Added dental sandbox account.

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+module "dentalsandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cashrail.dentaluser@gmail.com"
+    AccountName               = "cashrail-dentalsandbox"
+    ManagedOrganizationalUnit = "CashRails"
+    SSOUserEmail              = "cashrail.dentaluser@gmail.com"
+    SSOUserFirstName          = "Dental"
+    SSOUserLastName           = "Sandbox"
+  }
+
+  account_tags = {
+    "Purpose" = "Sandbox for Dental"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Amit"
+    change_reason       = "Create new account for dental sandbox"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
